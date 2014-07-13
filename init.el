f3f6333ae412516a19478add3ee7a371c3d18e64
Dmitry Andreychuk <and.dmitry@gmail.com>
2014-07-13 21:45:14 +0400

    Created separate file for Custom

--- init.el
+++ init.el
@@ -5,6 +5,11 @@
 
 (setq inhibit-startup-screen t)
 
+;; separate file for Custom
+;; TODO: use user-emacs-directory?
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
+
 ;;; repos and packages
 ;;; FIXME: fails to install packages
 (when (>= emacs-major-version 24)
@@ -153,17 +158,4 @@
 (global-set-key (kbd "C-x k") 'kill-this-buffer)
 
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(history-length 1000)
- '(x-select-enable-clipboard t))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
 (put 'dired-find-alternate-file 'disabled nil)
