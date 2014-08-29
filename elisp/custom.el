d1ae0e3144000316fa48287bd3e48b1756db0727
Dmitry Andreychuk <and.dmitry@gmail.com>
2014-08-29 19:08:18 +0400

    Adjusted terminal colors (testing)

--- elisp/custom.el
+++ elisp/custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector ["black" "orange red" "spring green" "gold" "blue" "magenta" "deep sky blue" "white"])
  '(history-length 1000)
  '(x-select-enable-clipboard t))
 (custom-set-faces
