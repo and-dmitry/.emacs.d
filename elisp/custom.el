c4f0a3475a272ae4c53c1c1bcecff4b2df937b6b
Dmitry Andreychuk <and.dmitry@gmail.com>
2015-08-11 08:44:53 +0300

    Set SGML offset to 4

--- elisp/custom.el
+++ elisp/custom.el
@@ -3,8 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector ["black" "orange red" "spring green" "gold" "blue" "magenta" "deep sky blue" "white"])
+ '(ansi-color-names-vector
+   ["black" "orange red" "spring green" "gold" "blue" "magenta" "deep sky blue" "white"])
  '(history-length 1000)
+ '(sgml-basic-offset 4)
  '(x-select-enable-clipboard t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
