b9f8442e9ef3e6adda8f6d5e7a750a7334e6f728
Dmitry Andreychuk <and.dmitry@gmail.com>
2018-10-07 20:53:45 +0300

    x-select-enable-clipboard is renamed

--- elisp/custom.el
+++ elisp/custom.el
@@ -16,8 +16,8 @@
  '(package-selected-packages
    (quote
     (yaml-mode which-key web-mode solarized-theme smex smartscan rg projectile nose nginx-mode magit log4j-mode groovy-mode grep-a-lot expand-region elfeed-org elfeed easy-kill beacon adoc-mode)))
+ '(select-enable-clipboard t)
  '(sgml-basic-offset 4)
- '(x-select-enable-clipboard t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
